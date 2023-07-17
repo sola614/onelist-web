0d149f6b16144098476b61e78c68da1c65c59af8
sola614 <36149249+sola614@users.noreply.github.com>
2023-07-17 15:15:49 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,3 @@
-/*
- * @Descripttion: 文件描述
- * @Author: sola.zhang
- * @Date: 2023-07-13 14:29:27
- * @LastEditors: sola.zhang
- * @LastEditTime: 2023-07-13 14:31:15
- */
 module.exports = {
   devServer: {
     port: 7853,
